e4cbd480f94ccbf5d862c9eddbb883a89234d5c1
Rahil Tariq <163430192+rahiltariq07@users.noreply.github.com>
2024-09-03 10:53:17 +0530

    Update script.js

--- New folder (3)/script.js
+++ New folder (3)/script.js
@@ -1,7 +1,3 @@
-document.getElementById('start-measuring').addEventListener('click', () => {
-    window.location.href = '/Start Measuring/index.html'
-});
-
-document.getElementById('get-started').addEventListener('click', () => {
-    window.location.href = '/Start Measuring/index.html'
-});
+function toggleMenu() {
+    document.querySelector('.navbar').classList.toggle('active');
+}
