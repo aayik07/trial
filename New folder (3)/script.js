d7a775d3d8ed5954a74c2248b821d45f7140d31f
Rahil Tariq <163430192+rahiltariq07@users.noreply.github.com>
2024-09-03 10:38:42 +0530

    Add files via upload

---
+++ New folder (3)/script.js
@@ -0,0 +1,7 @@
+document.getElementById('start-measuring').addEventListener('click', () => {
+    window.location.href = '/Start Measuring/index.html'
+});
+
+document.getElementById('get-started').addEventListener('click', () => {
+    window.location.href = '/Start Measuring/index.html'
+});
